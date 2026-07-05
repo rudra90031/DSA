16145906772159fdb7b75788bc59e6debf39627d
Rudra <rudraaamail@gmail.com>
2026-07-05 13:57:44 +0530

    Time: 9 ms (59.93%) | Memory: 11.5 MB (68.29%) - LeetSync

--- 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
+++ 1234-number-of-paths-with-max-score/number-of-paths-with-max-score.cpp
@@ -1,65 +1,73 @@
 class Solution {
 public:
     vector<int> pathsWithMaxScore(vector<string>& board) {
-        const int MOD = 1000000007;
+        int mod = 1e9 + 7;
         int n = board.size();
 
-        vector<int> nextScore(n + 1, -1);
-        vector<int> nextWays(n + 1, 0);
+        vector<vector<int>> maxScore(n, vector<int>(n, -1));
+        vector<vector<int>> ways(n, vector<int>(n, 0));
 
-        for (int i = n - 1; i >= 0; --i) {
-            vector<int> currScore(n + 1, -1);
-            vector<int> currWays(n + 1, 0);
+        maxScore[n - 1][n - 1] = 0;
+        ways[n - 1][n - 1] = 1;
 
-            for (int j = n - 1; j >= 0; --j) {
-                char cell = board[i][j];
-
-                if (cell == 'X') {
+        for(int i = n - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 0; j--){
+                if(board[i][j] == 'X'){
                     continue;
                 }
 
-                if (cell == 'S') {
-                    currScore[j] = 0;
-                    currWays[j] = 1;
+                if(i == n - 1 && j == n - 1){
                     continue;
                 }
 
-                int best = max({
-                    nextScore[j],
-                    currScore[j + 1],
-                    nextScore[j + 1]
-                });
+                int bestScore = -1;
+                long long totalWays = 0;
 
-                if (best == -1) {
+                if(i + 1 < n && maxScore[i + 1][j] != -1){
+                    if(maxScore[i + 1][j] > bestScore){
+                        bestScore = maxScore[i + 1][j];
+                        totalWays = ways[i + 1][j];
+                    }else if(maxScore[i + 1][j] == bestScore){
+                        totalWays = (totalWays + ways[i + 1][j]) % mod;
+                    }
+                }
+
+                if(j + 1 < n && maxScore[i][j + 1] != -1){
+                    if(maxScore[i][j + 1] > bestScore){
+                        bestScore = maxScore[i][j + 1];
+                        totalWays = ways[i][j + 1];
+                    }else if(maxScore[i][j + 1] == bestScore){
+                        totalWays = (totalWays + ways[i][j + 1]) % mod;
+                    }
+                }
+
+                if(i + 1 < n && j + 1 < n && maxScore[i + 1][j + 1] != -1){
+                    if(maxScore[i + 1][j + 1] > bestScore){
+                        bestScore = maxScore[i + 1][j + 1];
+                        totalWays = ways[i + 1][j + 1];
+                    }else if(maxScore[i + 1][j + 1] == bestScore){
+                        totalWays = (totalWays + ways[i + 1][j + 1]) % mod;
+                    }
+                }
+
+                if(bestScore == -1){
                     continue;
                 }
 
-                long long ways = 0;
-
-                if (nextScore[j] == best) {
-                    ways += nextWays[j];
-                }
-                if (currScore[j + 1] == best) {
-                    ways += currWays[j + 1];
-                }
-                if (nextScore[j + 1] == best) {
-                    ways += nextWays[j + 1];
+                int curr = 0;
+                if(board[i][j] >= '1' && board[i][j] <= '9'){
+                    curr = board[i][j] - '0';
                 }
 
-                int value = (cell == 'E') ? 0 : cell - '0';
-
-                currScore[j] = best + value;
-                currWays[j] = ways % MOD;
+                maxScore[i][j] = bestScore + curr;
+                ways[i][j] = totalWays % mod;
             }
-
-            nextScore = move(currScore);
-            nextWays = move(currWays);
         }
 
-        if (nextScore[0] == -1) {
+        if(maxScore[0][0] == -1){
             return {0, 0};
         }
 
-        return {nextScore[0], nextWays[0]};
+        return {maxScore[0][0], ways[0][0]};
     }
 };
