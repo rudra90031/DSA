fa1677335ef9a1ffa765e7dd800ac916b66d5967
Rudra <rudraaamail@gmail.com>
2026-07-16 06:52:49 +0530

    Time: 112 ms (5.53%) | Memory: 231.6 MB (7.22%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,27 +1,35 @@
+#include <vector>
+#include <algorithm>
+#include <numeric> // for gcd
+using namespace std;
+
 class Solution {
 public:
     long long gcdSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<long long> prefixGcd(n);
-
-        long long maxi = 0;
-
-        for(int i = 0; i < n; i++) {
-            maxi = max(maxi, (long long)nums[i]);
-            prefixGcd[i] = __gcd((long long)nums[i], maxi);
+        long long max_ele=0;
+        vector<long long> prefixGcd;
+        vector<long long> sample;
+        for(long long it : nums){
+            max_ele=max(max_ele,it);
+            sample.push_back(max_ele);
         }
-
-        sort(prefixGcd.begin(), prefixGcd.end());
-
-        long long ans = 0;
-        int l = 0, r = n - 1;
-
-        while(l < r) {
-            ans += __gcd(prefixGcd[l], prefixGcd[r]);
-            l++;
-            r--;
+        for(long long i=0;i<nums.size();i++){
+            long long x=gcd(sample[i],nums[i]);
+            prefixGcd.push_back(x);
         }
-
-        return ans;
+        sort(prefixGcd.begin(),prefixGcd.end());
+        long long left=0;
+        long long right=nums.size()-1;
+        vector<pair<long long,long long>> v;
+        while(left<right){
+            v.push_back({prefixGcd[left],prefixGcd[right]});
+            left++;
+            right--;
+        }
+        long long sum=0;
+        for(long long i=0;i<nums.size()/2;i++){
+            sum+=gcd(v[i].first,v[i].second);
+        }
+        return sum;
     }
 };
