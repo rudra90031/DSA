ead90cfdd83dc3902f2e847661c9614fad810d74
Rudra <rudraaamail@gmail.com>
2026-07-23 00:02:45 +0530

    Time: 161 ms (45.16%) | Memory: 283.3 MB (38.71%) - LeetSync

--- 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
+++ 3804-maximize-active-section-with-trade-ii/maximize-active-section-with-trade-ii.cpp
@@ -2,106 +2,89 @@ class Solution {
 public:
     vector<int> maxActiveSectionsAfterTrade(string s, vector<vector<int>>& queries) {
         int n = s.length();
-        int total_ones = 0;
-        for (char c : s) {
-            if (c == '1') total_ones++;
-        }
+        int ones_total = count(s.begin(), s.end(), '1');
         
-        vector<int> type;
-        vector<int> start;
-        vector<int> end_idx;
-        
-        for (int i = 0; i < n; ) {
-            int j = i;
-            while (j < n && s[j] == s[i]) {
-                j++;
-            }
-            type.push_back(s[i] - '0');
-            start.push_back(i);
-            end_idx.push_back(j - 1);
-            i = j;
-        }
-        
-        int N = type.size();
-        
-        vector<int> pos_to_seg(n);
-        for (int i = 0; i < N; i++) {
-            for (int j = start[i]; j <= end_idx[i]; j++) {
-                pos_to_seg[j] = i; 
+        vector<pair<int, int>> zero_runs;
+        int i = 0;
+        while (i < n) {
+            if (s[i] == '0') {
+                int start = i;
+                while (i < n && s[i] == '0') {
+                    i++;
+                }
+                zero_runs.push_back({start, i - 1});
+            } else {
+                i++;
             }
         }
         
-        vector<int> ans(N, 0);
-        for (int i = 1; i < N - 1; i++) {
-            if (type[i] == 1) {
-                ans[i] = (end_idx[i - 1] - start[i - 1] + 1) + (end_idx[i + 1] - start[i + 1] + 1);
+        vector<tuple<int, int, int, int, int>> pairs;
+        for (size_t k = 0; k + 1 < zero_runs.size(); ++k) {
+            auto [L1, R1] = zero_runs[k];
+            auto [L2, R2] = zero_runs[k+1];
+            pairs.push_back({L1, R1, L2, R2, (R1 - L1 + 1) + (R2 - L2 + 1)});
+        }
+        
+        int num_pairs = pairs.size();
+        if (num_pairs == 0) {
+            return vector<int>(queries.size(), ones_total);
+        }
+        
+        int K = bit_width((unsigned int)num_pairs);
+        vector<vector<int>> st(num_pairs, vector<int>(K, 0));
+        for (int j = 0; j < num_pairs; ++j) {
+            st[j][0] = get<4>(pairs[j]);
+        }
+        
+        for (int j = 1; j < K; ++j) {
+            for (int idx = 0; idx <= num_pairs - (1 << j); ++idx) {
+                st[idx][j] = max(st[idx][j-1], st[idx + (1 << (j-1))][j-1]);
             }
         }
         
-        vector<int> log_table(N + 1, 0);
-        for (int i = 2; i <= N; i++) {
-            log_table[i] = log_table[i / 2] + 1;
-        }
-        
-        int K = log_table[N] + 1;
-        vector<vector<int>> st(K, vector<int>(N, 0));
-        
-        for (int i = 0; i < N; i++) {
-            st[0][i] = ans[i];
-        }
-        
-        for (int j = 1; j < K; j++) {
-            for (int i = 0; i + (1 << j) <= N; i++) {
-                st[j][i] = max(st[j - 1][i], st[j - 1][i + (1 << (j - 1))]);
-            }
-        }
-        
-        auto queryRMQ = [&](int L_idx, int R_idx) {
-            if (L_idx > R_idx) return 0;
-            int j = log_table[R_idx - L_idx + 1];
-            return max(st[j][L_idx], st[j][R_idx - (1 << j) + 1]);
+        auto query_st = [&](int L, int R) {
+            if (L > R) return 0;
+            int j = bit_width((unsigned int)(R - L + 1)) - 1;
+            return max(st[L][j], st[R - (1 << j) + 1][j]);
         };
         
-        auto eval = [&](int i, int L, int R, int segL, int segR) {
-            if (i <= segL || i >= segR) return 0;
-            if (type[i] == 0) return 0;
-            
-            int left_len = 0;
-            if (i - 1 == segL) left_len = max(0, end_idx[i - 1] - L + 1);
-            else left_len = end_idx[i - 1] - start[i - 1] + 1;
-            
-            int right_len = 0;
-            if (i + 1 == segR) right_len = max(0, R - start[i + 1] + 1);
-            else right_len = end_idx[i + 1] - start[i + 1] + 1;
-            
-            return left_len + right_len;
-        };
+        vector<int> R1_list, L2_list;
+        for (auto& p : pairs) {
+            R1_list.push_back(get<1>(p));
+            L2_list.push_back(get<2>(p));
+        }
         
-        vector<int> res;
-        
-        for (const auto& q : queries) {
-            int L = q[0];
-            int R = q[1];
+        vector<int> ans;
+        for (auto& q : queries) {
+            int l = q[0], r = q[1];
+            int first_k = lower_bound(R1_list.begin(), R1_list.end(), l) - R1_list.begin();
+            int last_k = upper_bound(L2_list.begin(), L2_list.end(), r) - L2_list.begin() - 1;
             
-            int segL = pos_to_seg[L];
-            int segR = pos_to_seg[R];
-            
-            if (segR - segL < 2) {
-                res.push_back(total_ones);
+            if (first_k > last_k) {
+                ans.push_back(ones_total);
                 continue;
             }
             
-            int max_gain = 0;
-            max_gain = max(max_gain, eval(segL + 1, L, R, segL, segR));
-            max_gain = max(max_gain, eval(segR - 1, L, R, segL, segR));
-            
-            if (segL + 2 <= segR - 2) {
-                max_gain = max(max_gain, queryRMQ(segL + 2, segR - 2));
+            int best_gain = 0;
+            if (first_k == last_k) {
+                auto [L1, R1, L2, R2, _] = pairs[first_k];
+                int gain = (R1 - max(L1, l) + 1) + (min(R2, r) - L2 + 1);
+                best_gain = max(best_gain, gain);
+            } else {
+                auto [L1, R1, L2, R2, _] = pairs[first_k];
+                int gain1 = (R1 - max(L1, l) + 1) + (min(R2, r) - L2 + 1);
+                best_gain = max(best_gain, gain1);
+                
+                auto [L1_l, R1_l, L2_l, R2_l, _] = pairs[last_k];
+                int gain2 = (R1_l - max(L1_l, l) + 1) + (min(R2_l, r) - L2_l + 1);
+                best_gain = max(best_gain, gain2);
+                
+                if (first_k + 1 <= last_k - 1) {
+                    best_gain = max(best_gain, query_st(first_k + 1, last_k - 1));
+                }
             }
-            
-            res.push_back(total_ones + max_gain);
+            ans.push_back(ones_total + best_gain);
         }
-        
-        return res;
+        return ans;
     }
 };
