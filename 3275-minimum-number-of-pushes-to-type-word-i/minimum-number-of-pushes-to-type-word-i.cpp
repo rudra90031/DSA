56a3e0466e4695549191e5df9d0eb2073a78dd21
Rudra <rudraaamail@gmail.com>
2026-07-30 20:57:44 +0530

    Time: 3 ms (11.73%) | Memory: 8.3 MB (54.67%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    int minimumPushes(string word) {
-        int pushes = 0;
-
-        for (int i = 0; i < word.size(); i++) {
-
-            pushes += (i / 8) + 1;
-        }
-
-        return pushes;
+    int minimumPushes(string& word) {
+        auto [q, r]=div(word.size(), 8);
+        return r*(q+1)+q*(q+1)*4;
     }
 };
